89ecb62c275d2ee0ff116a24986b49978aca6464
Gregory Kimaru <gregorykariara1@gmail.com>
2026-05-01 12:42:57 +0300

    MOVING TO WEEK1

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,6 @@
 from django.urls import path
 from . import views
-from .marine import (
-    fetch_and_store_environmental_data,
-    fetch_water_level_data,
-    fetch_water_temperature,
-    fetch_conductivity,
-    fetch_currents,
-    fetch_solar,
-    fetch_all_environmental_data
-    )
+from .marine import fetch_all_environmental_data
 
 urlpatterns = [
 
@@ -19,12 +11,6 @@ urlpatterns = [
     path('observation/create/', views.observation_view, name='create_observation'),
     path('observation-media/create/', views.observation_media_view, name='create_observation_media'),
     path('shark-behaviour/create/', views.shark_behaviour, name='create_shark_behaviour'),
-    path('environment/', fetch_and_store_environmental_data, name='services'),
-    path("tide/", fetch_water_level_data, name='Tide Data'),
-    path("water_temp/", fetch_water_temperature, name='Water Temp'),
-    path("conductivity/", fetch_conductivity, name='conductivity'),
-    path("currents/", fetch_currents, name="Currents"),
-    path("solar/", fetch_solar, name="Astrological"),
     path("all/", fetch_all_environmental_data, name="everything")
     
 
